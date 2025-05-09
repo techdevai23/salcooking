740a5354a9255d2332c2c7f9a4fed319df62b3f3
Oscar Racero <racero@gmail.com>
2025-05-09 22:06:15 +0200

    trabajando en  cambiar el mheader de tamaño

--- header.php
+++ header.php
@@ -11,10 +11,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   
-    <!-- <link rel="stylesheet" href="styles.css"> -->
+    <!-- <link rel="stylesheet" href=" styles/styles.css"> -->
      <!-- hago un truco para evitar problemas de caché que pueden volver loco pensando que no
       se actualizan los cambios -->
-    <link rel="stylesheet" href="styles.css?v=<?php echo filemtime('styles.css'); ?>">
+    <link rel="stylesheet" href="styles/styles.css?v=<?php echo filemtime('styles/styles.css'); ?>">
 <script src="scripts/descargarListaPDF.js?v=<?php echo filemtime('scripts/descargarListaPDF.js'); ?>"></script>
 
     <!-- cargo hoja de estilos propias -->
@@ -26,7 +26,7 @@
 <body>
     <header class="main-header">
         <div class="logo-cabecera">
-            <a href="index.php"><img src="images//logos/cabecera.png" alt="SalCooking cabecera Logo"></a>
+            <a href="index.php"><img src="images/logos/cabecera.png" alt="SalCooking cabecera Logo"></a>
         </div>
         <nav class="top-nav mb-2">
             <div class="container">
--- views/header.php
+++ views/header.php
@@ -10,7 +10,7 @@
     <title>SalCooking - Menús personalizados e inteligentes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href=" styles/styles.css">
     <!-- cargo hoja de estilos propias -->
     <?php if (isset($css_extra)) echo $css_extra; ?>
 
