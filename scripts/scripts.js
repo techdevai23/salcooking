3c32ec5729528e42f0f23a3993c2c740a46d2017
Oscar Racero <racero@gmail.com>
2025-05-09 22:05:50 +0200

    cambios in working y de ubicacion es styles y script general

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    // Mobile menu toggle
+    // Alternar menú móvil
     $('.mobile-menu-toggle').click(function() {
         $('.mobile-nav').toggleClass('active');
     });
 
-    // Close mobile menu when clicking outside
+    // Cerrar el menú móvil al hacer clic fuera
     $(document).click(function(event) {
         if (!$(event.target).closest('.mobile-menu-toggle, .mobile-nav').length) {
             $('.mobile-nav').removeClass('active');
@@ -27,16 +27,11 @@ $(document).ready(function() {
     // Agregar clase activa a la página actual en la navegación
     const currentPage = window.location.pathname.split('/').pop();
     
-    // Si la página actual es la raíz o index.html, activa el primer elemento
     if (currentPage === '' || currentPage === 'index.html') {
         $('.nav-tabs li:first-child').addClass('active');
         $('.mobile-nav li:first-child').addClass('active');
     } else {
-        // Agregar clase activa a la pestaña de navegación correspondiente
         $(`.nav-tabs li a[href="${currentPage}"]`).parent().addClass('active');
         $(`.mobile-nav li a[href="${currentPage}"]`).parent().addClass('active');
-        // y quitar la clase activa de la pestaña de navegación principal
-        $(`.nav-tabs li a[href="${currentPage}"]`).parent().siblings().removeClass('active');
-        $(`.mobile-nav li a[href="${currentPage}"]`).parent().siblings().removeClass('active');
     }
 });
