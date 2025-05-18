346504245af5e71987359a8d364943a56ff80a5e
Oscar Racero <racero@gmail.com>
2025-05-18 20:04:05 +0200

    arreglado en categorias las tarjetas

--- recetas-categoria.php
+++ recetas-categoria.php
@@ -33,44 +33,63 @@ $css_extra .= '<link rel="stylesheet" href="styles/trucos.css?v=' . filemtime('s
  <p>Desde un desayuno energético para empezar el día, hasta el postre más dulce para acabarlo con una sonrisa. En esta sección encontrarás recetas saludables clasificadas por tipo de plato: ideas rápidas y nutritivas para el desayuno, entrantes ligeros que despiertan el apetito, platos principales completos y equilibrados, y postres que combinan sabor y bienestar. Cada tarjetón te llevará a un mundo de opciones adaptadas a tus gustos, necesidades y estilo de vida. ¡Elige por dónde empezar tu próxima comida saludable!</p>
 
                   </div>
-                 
+                
                   <article class="tarjeta-truco receta">
+                    
                         <img class="foto-truco" src="sources/platos/id41.png" alt="foto Desayuno" style="width: 150px;">
+                        
                         <div class="texto-truco">
+                          
                             <h2>Desayuno</h2>
                             <div class="tip-content">
+                               <a href="resultado-recetas.php" class="tarjeta-receta">
                                 <p>Empieza el día con energía y equilibrio. En nuestra categoría de desayunos encontrarás opciones saludables y deliciosas como porridge de avena sin gluten, tortitas de plátano y tostadas integrales con aguacate. Platos diseñados para nutrir cuerpo y mente desde la primera comida del día, aptos para todos los gustos y necesidades alimentarias.</div>
-                        </div>
+                       </a>
+                              </div>
+                    
                     </article>
+                   
 
                     <article class="tarjeta-truco receta" >
+                      
                         <img class="foto-truco" src="sources/platos/entrante1.png" alt="fot entrante" style="width: 150px;">
                         <div class="texto-truco">
                             <h2>Entrante</h2>
                             <div class="tip-content">
+                              <a href="resultado-recetas.php" class="tarjeta-receta">
                                 <p>Abre el apetito con ideas ligeras y llenas de sabor. Desde ensaladas frescas hasta cremas suaves o rollitos vegetales, los entrantes de SalCooking están pensados para sorprender sin saturar. Son el complemento perfecto para comenzar una comida completa y saludable, adaptada a tus restricciones o intolerancias.</p>
-                            </div>
+                            </a>
+                              </div>
                         </div>
+                     
                     </article>
 
                     <article class="tarjeta-truco receta">
+                     
                         <img class="foto-truco" src="sources/platos/principal1.png"" alt="fotro principal" style="width: 150px;">
                         <div class="texto-truco">
                             <h2>Plato principal</h2>
                             <div class="tip-content">
+                               <a href="resultado-recetas.php" class="tarjeta-receta">
                                 <p>Descubre recetas completas, equilibradas y adaptadas a distintos estilos de vida. Platos como lasañas de verduras, guisos veganos, carnes al horno o pescados con acompañamientos naturales, forman parte de esta selección pensada para cuidar tu salud sin renunciar al sabor. Perfectos para almuerzos o cenas nutritivas.</p>
-                            </div>
+                               </a>
+                              </div>
                         </div>
+                       
                     </article>
 
                     <article class="tarjeta-truco receta">
+                     
                         <img class="foto-truco" src="sources/platos/postre3.png" alt="foto postre" style="width: 150px;">
                         <div class="texto-truco">
                             <h2>Postre</h2>
                             <div class="tip-content">
+                               <a href="resultado-recetas.php" class="tarjeta-receta">
                                 <p>El toque final ideal para cualquier menú. Aquí te esperan postres dulces y saludables como puddings de chía, parfaits con fruta fresca o bizcochos sin gluten ni azúcares añadidos. Disfruta del placer sin culpa con nuestras propuestas adaptadas incluso para personas con diabetes, colesterol o alergias.</p>
-                            </div>
+                               </a>
+                              </div>
                         </div>
+                   
                     </article>
 
                   
