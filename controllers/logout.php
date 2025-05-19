411a1c3638276f8d0b0bc72e2fc72941db7280b7
Oscar Racero <racero@gmail.com>
2025-05-19 21:18:15 +0200

    todo bien antes de modificar cambio-pass y login, para que login reconozca contraseña y puedas acceder a olvide contraseña

--- controllers/logout.php
+++ controllers/logout.php
@@ -1,11 +1,8 @@
 <?php
 session_start(); // Acceder a la sesión
-
 // Destruir todas las variables de sesión.
 $_SESSION = array();
-
 // Si se desea destruir la sesión completamente, borre también la cookie de sesión.
-// Nota: ¡Esto destruirá la sesión, y no solo los datos de la sesión!
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -13,11 +10,10 @@ if (ini_get("session.use_cookies")) {
         $params["secure"], $params["httponly"]
     );
 }
-
 // Finalmente, destruir la sesión.
 session_destroy();
 
-// Redirigir a la página de login o a la de inicio
-header("Location: login.php");
+// Redirigir a la página de inicio
+header("Location: index.php");
 exit();
 ?>
